f383264a7e0614583db4335037d4c99624934ece
Andrea Mckenzie <ma3east@gmail.com>
2015-09-08 16:35:19 +0100

    including dami's code

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 angular
-.module("eventMatchApp", ['angular-jwt', 'ngResource', 'ui.router'])
+.module("eventMatchApp", ['angular-jwt', 'ngResource', 'ui.router', 'jcs-autoValidate', 'angular-ladda'])
 .constant('API', 'http://localhost:3000/api')
 .config(MainRouter);
 
@@ -9,6 +9,14 @@ angular
   var token = $window.localStorage.getItem('token');
   $http.defaults.headers.common['Authorization'] = 'Bearer'
 })
+.run(function (defaultErrorMessageResolver) {
+    defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+      errorMessages['tooYoung'] = 'You must be at least {0} years old to use this site';
+      errorMessages['tooOld'] = 'You must be max {0} years old to use this site';
+      errorMessages['badUsername'] = 'Username can only contain numbers and letters and _';
+    });
+  }
+);
 
 function MainRouter($stateProvider, $urlRouterProvider) {
   $stateProvider  
