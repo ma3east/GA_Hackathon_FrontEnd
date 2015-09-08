5c28b97205d9e6bf41a45ff51df77a6949167e7a
Andrea Mckenzie <ma3east@gmail.com>
2015-09-08 18:56:59 +0100

    adding changes before pulling latest master

--- public/js/controllers/eventsController.js
+++ public/js/controllers/eventsController.js
@@ -2,8 +2,21 @@ angular
 .module("eventMatchApp")
   .controller("eventsController", EventController);
 
-EventController.$inject = ['Event'];  
+EventController.$inject = ['Event']; 
+
 function EventController(Event){
-  this.all = Event.query();
+  // this.all = Event.query();
+
+  var self = this;
+  self.all = [];
+
+  self.newEvent = {};
+  self.getEvents = getEvents;
+
+  getEvents();
+  function getEvents(){
+
+  }
+
 
 }  
--- public/js/models/event.js
+++ public/js/models/event.js
@@ -4,9 +4,21 @@ angular
 
  Event.$inject = ['$resource'];
  function Event ($resource) {
-  var EventResource = $resource('http://172.19.5.100:9000/api/events/:id', {id: '@_id'}, {
+  var url = 'http://localhost:9000/api/'
+  var EventResource = $resource(url + 'events/search', {id: '@_id'}, {
     'update': { method: 'PUT' }
 
   });
+
+  
   return EventResource; 
  }
+
+
+
+
+
+ // var EventResource = $resource('http://172.19.5.100:9000/api/events/search', {id: '@_id'}, {
+ //   'update': { method: 'PUT' }
+
+ // });
